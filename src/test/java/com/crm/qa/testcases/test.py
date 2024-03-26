e16fc26d4ac16a28d089cb853359658d01258e8a
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-03-27 02:41:25 +0500

    Update test.py

--- src/test/java/com/crm/qa/testcases/test.py
+++ src/test/java/com/crm/qa/testcases/test.py
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTest {
+public class SimpleSeleniumTest {
     public static void main(String[] args) {
         // Set the path to the ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
@@ -9,19 +9,12 @@ public class SeleniumTest {
         // Create a new instance of the Chrome driver
         WebDriver driver = new ChromeDriver();
 
-        // Navigate to a website
-        driver.get("https://www.example.com");
+        // Navigate to Google
+        driver.get("https://www.google.com");
 
         // Get and print the title of the current page
         String pageTitle = driver.getTitle();
         System.out.println("Page title is: " + pageTitle);
-
-        // Verify the title of the page
-        if (pageTitle.equals("Example Domain")) {
-            System.out.println("Title verification successful");
-        } else {
-            System.out.println("Title verification failed");
-        }
 
         // Close the browser
         driver.quit();
